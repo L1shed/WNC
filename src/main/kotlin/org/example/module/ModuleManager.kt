a6e075e5e2219c0a26e0dad05c9ea19dcbe4649a
exejar <56853437+exejar@users.noreply.github.com>
2023-09-26 21:50:30 -0400

    Created KeyboardEvent listener to toggle modules

--- src/main/kotlin/org/example/module/ModuleManager.kt
+++ src/main/kotlin/org/example/module/ModuleManager.kt
@@ -1,9 +1,18 @@
 package org.example.module
 
+import net.weavemc.loader.api.event.KeyboardEvent
+import net.weavemc.loader.api.event.SubscribeEvent
+
 object ModuleManager {
     var moduleMap: HashMap<String, Module> = hashMapOf(
 
     )
 
     fun getModule(name: String): Module = moduleMap[name] ?: error("Failed to retrieve module $name")
+
+    @SubscribeEvent
+    fun onKeyPress(event: KeyboardEvent) {
+        if (event.keyState)
+            moduleMap.values.find { it.keybind == event.keyCode }?.toggle() ?: return
+    }
 }
