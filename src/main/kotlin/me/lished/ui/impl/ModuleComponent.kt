82b523f9c430488ce743d10ad35fafa500545ae1
BuildTools <skywarspanel@gmail.com>
2024-03-01 21:47:53 +0100

    Remove old code by sassan

--- src/main/kotlin/me/lished/ui/impl/ModuleComponent.kt
+++ src/main/kotlin/me/lished/ui/impl/ModuleComponent.kt
@@ -8,14 +8,6 @@ import net.minecraft.client.gui.Gui
 class ModuleComponent(private val module: Module, x: Int, y: Int, width: Int, height: Int) :
     Component(x, y, width, height) {
     override fun drawScreen(mouseX: Int, mouseY: Int) {
-//        RenderUtils.drawRect(x, y, width, height, -0xe0e0e1)
-//        RenderUtils.drawString(
-//            Minecraft.getMinecraft().fontRendererObj,
-//            module.name,
-//            x + 6,
-//            (y + Minecraft.getMinecraft().fontRendererObj.FONT_HEIGHT.toFloat() / 2 + 4) as Int,
-//            if (module.isEnabled()) -0xb0b1 else -1
-//        )
         Gui.drawRect(x, y, x + width, y + height, -0xe0e0e1)
         Minecraft.getMinecraft().fontRendererObj.drawString(
             module.name,
