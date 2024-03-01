3d3633b24559f8837dabdb3bd60ff2a0393cfdc3
BuildTools <skywarspanel@gmail.com>
2024-03-01 21:21:10 +0100

    d

---
+++ src/main/kotlin/me/lished/module/impl/LeftClicker.kt
@@ -0,0 +1,27 @@
+package me.lished.module.impl
+
+import me.lished.module.Module
+import me.lished.setting.DoubleSliderSetting
+import me.lished.util.mc
+import net.weavemc.loader.api.event.SubscribeEvent
+import net.weavemc.loader.api.event.TickEvent
+import org.lwjgl.input.Keyboard
+
+class LeftClicker(
+    private val cps: DoubleSliderSetting = DoubleSliderSetting("CPS", "Attacks per second", emptyArray(),
+        0.0, 20.0, 1.0, 20.0, 0.1
+    ),
+): Module(
+    "LeftClicker",
+    "Automatically clicks",
+    Keyboard.KEY_NONE,
+    arrayOf(
+        cps
+    )
+) {
+    @SubscribeEvent
+    fun onTick(event: TickEvent.Post) {
+        enabled = true
+        mc.thePlayer.sendChatMessage("d")
+    }
+}
